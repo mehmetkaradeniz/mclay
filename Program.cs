bcc046c1180c5a9b10c11fea844d780a10ec40ef
Mehmet Karadeniz <mehmetkz61@gmail.com>
2025-03-02 13:42:55 +0300

    Version 0.3 - Feature: Playable mclay game - Feature: Display remaining mclays and display message when user is out of mclays - Feature: Bag: show earned items in bag - Feature: Cheat mode: %100 Win rate for all rounds

---
+++ Program.cs
@@ -0,0 +1,42 @@
+using mclay.Components;
+using Microsoft.FluentUI.AspNetCore.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+
+#region FLuentUI
+
+builder.Services.AddFluentUIComponents();
+
+// This required for app to not crash, see: https://github.com/microsoft/fluentui-blazor/issues/2460
+builder.Services.AddSingleton(new LibraryConfiguration()
+{
+    CollocatedJavaScriptQueryString = null,
+});
+
+#endregion
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
