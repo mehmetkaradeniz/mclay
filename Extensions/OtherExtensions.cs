397f164bf98779bf1a7bbae46219fe3356ca7533
Mehmet Karadeniz <mehmetkz61@gmail.com>
2025-03-28 18:09:05 +0300

    Generate Random Mclay table on page load

---
+++ Extensions/OtherExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace mclay.Extensions
+{
+    public static class OtherExtensions
+    {
+
+        public static T Random<T>(this List<T> list)
+        {
+            if (list.Count == 0)
+                throw new Exception("List is empty");
+
+            var rng = new Random();
+            var randomIndex = rng.Next(list.Count - 1);
+            var randomItem = list[randomIndex];
+
+            return randomItem;
+        }
+    }
+}
